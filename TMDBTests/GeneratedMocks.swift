141034e3b228939bfed186238e020ab2a804b215
Tuyen <tuyendle92@gmail.com>
2020-06-07 16:54:05 -0500

    fix travis

--- TMDBTests/GeneratedMocks.swift
+++ TMDBTests/GeneratedMocks.swift
@@ -1,4 +1,4 @@
-// MARK: - Mocks generated from file: TMDB/Services/Session.swift at 2020-06-07 20:18:43 +0000
+// MARK: - Mocks generated from file: TMDB/Services/Session.swift at 2020-06-07 21:53:06 +0000
 
 //
 //  Session.swift
@@ -104,7 +104,7 @@ import Foundation
 }
 
 
-// MARK: - Mocks generated from file: TMDB/Services/URLRequestBuilder.swift at 2020-06-07 20:18:43 +0000
+// MARK: - Mocks generated from file: TMDB/Services/URLRequestBuilder.swift at 2020-06-07 21:53:06 +0000
 
 //
 //  URLRequestBuilder.swift

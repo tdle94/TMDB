65ec71340467ad97f0e0938e3fab89a26aabeb66
Tuyen <tuyendle92@gmail.com>
2020-06-10 20:34:47 -0500

    add location manager class

--- TMDB/LocationManager.swift
+++ TMDB/LocationManager.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, CLLocationManagerDelegate {
+    var manager: CLLocationManager = CLLocationManager()
+    
+    var geodecoder: CLGeocoder = CLGeocoder()
+    
+    var setting: TMDBUserSettingProtocol = TMDBUserSetting()
+
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else {
+            return
+        }
+
+        geodecoder.reverseGeocodeLocation(location) { placemarks, error in
+            guard
+                error == nil,
+                let countryCode = placemarks?.first?.isoCountryCode else { return }
+
+            self.setting.country = countryCode
+            self.setting.language = Locale.current.languageCode
+            self.setting.region = Locale.current.regionCode
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            manager.startUpdatingLocation()
+        }
+    }
+}

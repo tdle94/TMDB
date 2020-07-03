4e60e1af84008caa2312858065ee3ccab39c9eee
Tuyen <tuyendle92@gmail.com>
2020-07-03 10:18:11 -0500

    Add PopularHeaderCell

---
+++ TMDB/TMDBViews/TMDBPreviewItemCell.swift
@@ -0,0 +1,91 @@
+//
+//  PopularItemCell.swift
+//  TMDB
+//
+//  Created by Tuyen Le on 6/16/20.
+//  Copyright © 2020 Tuyen Le. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import RealmSwift
+import SDWebImage
+
+class TMDBPreviewItemCell: UICollectionViewCell {
+    @IBOutlet weak var ratingLabel: TMDBCircleUserRating!
+    @IBOutlet weak var stackViewTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageView: UIImageView! {
+        didSet {
+            imageView.sd_imageIndicator = SDWebImageActivityIndicator.gray
+            imageView.roundImage()
+        }
+    }
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var subTitle: UILabel!
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        stackViewTopConstraint.constant = 0
+        imageView.image = nil
+        imageView.isHidden = false
+        ratingLabel.isHidden = true
+        title.text = ""
+        subTitle.text = ""
+    }
+
+    private func getImage(from path: String?, repository: TMDBRepositoryProtocol) {
+        guard
+            let path = path,
+            let url = repository.getImageURL(from: path) else {
+                imageView.image = UIImage(named: "NoImage")
+                return
+        }
+        imageView.sd_setImage(with: url, placeholderImage: nil, options: .init(rawValue: 0))
+    }
+
+    func configure(item: Object, with repository: TMDBRepositoryProtocol) {
+        if let item = item as? Movie ?? (item as? Trending)?.movie {
+            ratingLabel.isHidden = false
+            ratingLabel.rating = item.voteAverage
+            stackViewTopConstraint.constant = 10
+            title.text = item.originalTitle
+            subTitle.text = item.releaseDate
+            getImage(from: item.posterPath, repository: repository)
+        } else if let item = item as? TVShow ?? (item as? Trending)?.tv {
+            ratingLabel.isHidden = false
+            ratingLabel.rating = item.voteAverage
+            stackViewTopConstraint.constant = 10
+            title.text = item.originalName
+            subTitle.text = item.firstAirDate
+            getImage(from: item.posterPath, repository: repository)
+        } else if let item = item as? People ?? (item as? Trending)?.people {
+            title.text = item.name
+            subTitle.text = item.knownForDepartment
+            getImage(from: item.profilePath, repository: repository)
+        } else if let item = item as? ProductionCompany {
+            title.textAlignment = .center
+            title.text = ""
+            imageView.contentMode = .scaleAspectFit
+            imageView.layer.borderColor = .none
+            imageView.layer.borderWidth = 0
+            stackViewTopConstraint.constant = 0
+            if let path = item.logoPath {
+                getImage(from: path, repository: repository)
+            } else {
+                stackViewTopConstraint.constant = -40
+                title.text = item.name
+                imageView.isHidden = true
+            }
+        } else if let item = item as? Cast {
+            title.text = item.name
+            subTitle.text = item.character
+            getImage(from: item.profilePath, repository: repository)
+        } else if let item = item as? Crew {
+            title.text = item.name
+            subTitle.text = item.job
+            
+            getImage(from: item.profilePath, repository: repository)
+        }
+    }
+}

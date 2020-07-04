18544d495f1ce466d20d68402c024ef538db7838
Tuyen <tuyendle92@gmail.com>
2020-07-04 11:27:25 -0500

    add func for navigating to video webView

--- TMDB/TMDBCoordinator/MainCoordinator.swift
+++ TMDB/TMDBCoordinator/MainCoordinator.swift
@@ -8,12 +8,13 @@
 
 import Foundation
 import UIKit
+import AVKit
 
 struct MainCoordinator: Coordinator {
     var navigationController: UINavigationController
     var childCoordinators: [Coordinator] = []
     let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-    
+
     func navigateToMovieDetail(id: Int) {
         let movieDetailVC = storyboard.instantiateViewController(identifier: Constant.ViewControllerIdentifier.tmdbMovieDetail) as! TMDBMovieDetailViewController
         movieDetailVC.movieId = id
@@ -21,5 +22,11 @@ struct MainCoordinator: Coordinator {
         movieDetailVC.navigationItem.backBarButtonItem = UIBarButtonItem(title: nil, style: .plain, target: nil, action: nil)
         movieDetailVC.navigationItem.backBarButtonItem?.tintColor = Constant.Color.backgroundColor
         navigationController.pushViewController(movieDetailVC, animated: true)
+    }
+
+    func navigateToVideoPlayer(with videoURL: URL) {
+        let videoPlayerVC = storyboard.instantiateViewController(identifier: Constant.ViewControllerIdentifier.tmdbVideoPlayer) as! TMDBVideoPlayerViewController
+        videoPlayerVC.url = videoURL
+        navigationController.present(videoPlayerVC, animated: true, completion: nil)
     }
 }

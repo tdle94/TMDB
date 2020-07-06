bf98bfd77e5bf653b24aafa2c365b3b63cc21a1d
Tuyen <tuyendle92@gmail.com>
2020-07-06 09:23:44 -0500

    add TMDBMovieKeywordCell and TMDBMovieKeywordLayout

--- TMDB/TMDBViews/TMDBMovieKeywordCell.swift
+++ TMDB/TMDBViews/TMDBMovieKeywordCell.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+import UIKit
+
+class TMDBMovieKeywordCell: UICollectionViewCell {
+    let label = UILabel()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        label.frame = CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height)
+        contentView.addSubview(label)
+        layer.cornerRadius = 5
+        layer.borderColor = UIColor.black.cgColor
+        layer.borderWidth = 0.3
+        label.textColor = .darkGray
+        label.textAlignment = .center
+        label.font = UIFont(name: "Circular-Book", size: UIFont.smallSystemFontSize)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    func configure(keyword: Keyword) {
+        label.text = keyword.name
+    }
+}

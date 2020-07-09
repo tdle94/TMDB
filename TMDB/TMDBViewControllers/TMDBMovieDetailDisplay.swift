e55276a19f70dc170419d869a54289c7cf8fa149
Tuyen <tuyendle92@gmail.com>
2020-07-09 08:51:25 -0500

    add display review button function

--- TMDB/TMDBViewControllers/TMDBMovieDetailDisplay.swift
+++ TMDB/TMDBViewControllers/TMDBMovieDetailDisplay.swift
@@ -19,6 +19,7 @@ protocol TMDBMovieDetailDisplayProtocol {
     func displayOverviewDetail(label: UILabel, movie: Movie)
     func displayRuntime(label: UILabel, movie: Movie)
     func displayGenere(label: UILabel, movie: Movie)
+    func displayReviewButton(button: UIButton, movie: Movie)
 }
 
 class TMDBMovieDetailDisplay: TMDBMovieDetailDisplayProtocol {
@@ -46,6 +47,16 @@ class TMDBMovieDetailDisplay: TMDBMovieDetailDisplayProtocol {
     
     func displayTitle(label: UILabel, movie: Movie) {
         label.attributedText = NSAttributedString(string: movie.originalTitle, attributes: [NSAttributedString.Key.font: UIFont(name: "Circular-Book", size: UIFont.labelFontSize)!])
+    }
+    
+    func displayReviewButton(button: UIButton, movie: Movie) {
+        guard let reviews = movie.reviews?.reviews, !reviews.isEmpty else {
+            button.isHidden = true
+            return
+        }
+        let attributedString = NSAttributedString(string: NSLocalizedString("Review", comment: "") + " (\(reviews.count))",
+                                                  attributes: [NSAttributedString.Key.font: UIFont(name: "Circular-Book", size: UIFont.smallSystemFontSize)!])
+        button.setAttributedTitle(attributedString, for: .normal)
     }
     
     func displayOverview(label: UILabel) {

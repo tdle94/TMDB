6caaad13edc1e1d2b28585fcec73f0990dbb8db2
Tuyen <tuyendle92@gmail.com>
2020-07-14 11:04:08 -0500

    add person detail view controller

--- TMDB/TMDBViewControllers/TMDBPersonDetailViewController.swift
+++ TMDB/TMDBViewControllers/TMDBPersonDetailViewController.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+import UIKit
+
+class TMDBPersonDetailViewController: UIViewController {
+    var repository: TMDBRepositoryProtocol!
+    var personId: Int?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        repository = TMDBRepository(services: TMDBServices(session: TMDBSession(session: URLSession.shared),
+                                                           urlRequestBuilder: TMDBURLRequestBuilder()),
+                                    localDataSource: TMDBLocalDataSource(),
+                                    userSetting: TMDBUserSetting())
+    }
+
+    func getPersonDetail() {
+        guard let id = personId else { return }
+        repository.getPersonDetail(id: id) { result in
+            switch result {
+            case .failure(let error):
+                debugPrint(error.localizedDescription)
+            case .success(let person):
+                break
+            }
+        }
+    }
+}

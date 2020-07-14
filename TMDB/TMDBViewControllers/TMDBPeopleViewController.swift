22946ddf8f87481b6875edd18db0ae04933ff162
Tuyen <tuyendle92@gmail.com>
2020-07-14 17:34:58 -0500

    add people view controller

--- TMDB/TMDBViewControllers/TMDBPeopleViewController.swift
+++ TMDB/TMDBViewControllers/TMDBPeopleViewController.swift
@@ -2,8 +2,19 @@
 //  TMDBPeopleViewController.swift
 //  TMDB
 //
-//  Created by Tuyen Le on 7/14/20.
+//  Created by Tuyen Le on 6/9/20.
 //  Copyright © 2020 Tuyen Le. All rights reserved.
 //
 
 import Foundation
+import UIKit
+
+class TMDBPeopleViewController: UIViewController {
+    // MARK: - coordinator
+    var coordinator: MainCoordinator?
+
+    // MARK: - overrides
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

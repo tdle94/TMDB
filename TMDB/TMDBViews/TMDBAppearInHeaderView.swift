717e34ea9f2f54c0f6e537433e20190b1b28fb86
Tuyen <tuyendle92@gmail.com>
2020-07-15 14:56:58 -0500

    add appear in header view

--- TMDB/TMDBViews/TMDBAppearInHeaderView.swift
+++ TMDB/TMDBViews/TMDBAppearInHeaderView.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+import UIKit
+
+class TMDBAppearInHeaderView: TMDBPreviewHeaderView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        label.text = NSLocalizedString("Appear In", comment: "")
+        segmentControl.insertSegment(withTitle: NSLocalizedString("Movies", comment: ""), at: 0, animated: true)
+        segmentControl.insertSegment(withTitle: NSLocalizedString("TV Shows", comment: ""), at: 1, animated: true)
+        segmentControl.selectedSegmentIndex = 0
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}

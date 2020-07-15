40f9fb04ee87e848fb46121ea4e64a92439e9870
Tuyen <tuyendle92@gmail.com>
2020-07-15 14:57:31 -0500

    add getTVCredtis and getMovieCredits

--- TMDB/TMDBRepository/TMDBRepository.swift
+++ TMDB/TMDBRepository/TMDBRepository.swift
@@ -23,6 +23,8 @@ protocol TMDBRepositoryProtocol {
     // MARK: - people
     func getPopularPeople(page: Int, completion: @escaping (Result<PeopleResult, Error>) -> Void)
     func getPersonDetail(id: Int, completion: @escaping (Result<People, Error>) -> Void)
+    func getTVCredits(from personId: Int) -> TVCredit?
+    func getMovieCredits(from personId: Int) -> MovieCredit?
 
     // MARK: - tv shows
     func getPopularOnTV(page: Int, completion: @escaping (Result<TVShowResult, Error>) -> Void)
@@ -248,6 +250,14 @@ class TMDBRepository: TMDBRepositoryProtocol {
                 }
             }
         }
+    }
+
+    func getTVCredits(from personId: Int) -> TVCredit? {
+        return localDataSource.getPerson(id: personId)?.tvCredits
+    }
+
+    func getMovieCredits(from personId: Int) -> MovieCredit? {
+        return localDataSource.getPerson(id: personId)?.movieCredits
     }
 
     // MARK: - images

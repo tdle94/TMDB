b404539a063e5b1b65c1a002a1c8eaa1421d0f9b
Tuyen <tuyendle92@gmail.com>
2020-07-22 12:11:12 -0500

    add tv show detail subservice

--- TMDB/TMDBServices/SubServices/TMDBTVService.swift
+++ TMDB/TMDBServices/SubServices/TMDBTVService.swift
@@ -10,4 +10,5 @@ import Foundation
 
 protocol TMDBTVService {
     func getPopularOnTV(page: Int, completion: @escaping (Result<TVShowResult, Error>) -> Void)
+    func getTVShowDetail(id: Int, completion: @escaping (Result<TVShow, Error>) -> Void)
 }

e29897ec87dbcdfdc06463ca300c6d519d7d31ba
Tuyen <tuyendle92@gmail.com>
2020-07-23 17:27:45 -0500

    change query item

--- TMDB/TMDBServices/TMDBURLRequestBuilder.swift
+++ TMDB/TMDBServices/TMDBURLRequestBuilder.swift
@@ -53,7 +53,7 @@ struct TMDBURLRequestBuilder: TMDBURLRequestBuilderProtocol {
     func getTVShowDetailURLRequest(id: Int, language: String?) -> URLRequest {
         let queryItems = [
             URLQueryItem(name: "language", value: language ?? "en-US"),
-            URLQueryItem(name: "append_to_response", value: "keywords")
+            URLQueryItem(name: "append_to_response", value: "keywords,similar,recommendations")
             
         ]
         return buildURLRequest(path: "/3/tv/\(id)", queryItems: queryItems)

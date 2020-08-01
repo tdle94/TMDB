b9c833f9c3c312c156983cf469c3f07e2ed7d025
Tuyen <tuyendle92@gmail.com>
2020-08-01 14:26:11 -0500

    add tests

--- TMDBTests/TMDBServiceJsonData/TVShowSeasonDetail.swift
+++ TMDBTests/TMDBServiceJsonData/TVShowSeasonDetail.swift
@@ -7,3 +7,268 @@
 //
 
 import Foundation
+
+let tvShowSeasonDetail = Data("""
+    {
+    "_id": "5257539a19c29531db119ad2",
+    "air_date": "2007-11-15",
+    "episodes": [
+    {
+      "air_date": "2007-11-15",
+      "episode_number": 1,
+      "id": 256902,
+      "name": "Episode One",
+      "overview": "In the first episode we get to meet the estate agents and find out about Danny's unique approach to the business. Today Danny's clients are a couple who aren't what they seem on face value and Danny picks up on this.",
+      "production_code": "",
+      "season_number": 1,
+      "show_id": 3624,
+      "still_path": null,
+      "vote_average": 0,
+      "vote_count": 0,
+      "crew": [
+        {
+          "id": 1234395,
+          "credit_id": "5257539c19c29531db119af1",
+          "name": "Cilla Ware",
+          "department": "Directing",
+          "job": "Director",
+          "gender": 0,
+          "profile_path": null
+        }
+      ],
+      "guest_stars": [
+        {
+          "id": 7063,
+          "name": "Nina Sosanya",
+          "credit_id": "574b38dbc3a3683561000379",
+          "character": "",
+          "order": 1,
+          "gender": 1,
+          "profile_path": "/nQ0f0BHnEtRB4mJZsaRGScf9OL3.jpg"
+        },
+        {
+          "id": 984,
+          "name": "Nicholas Rowe",
+          "credit_id": "574b39109251412d29001a24",
+          "character": "",
+          "order": 3,
+          "gender": 2,
+          "profile_path": "/mAR7cSZO9V9v0fFlDxiKDfvHlkQ.jpg"
+        },
+        {
+          "id": 176201,
+          "name": "Grant Masters",
+          "credit_id": "574b394e925141362d001c8e",
+          "character": "",
+          "order": 7,
+          "gender": 2,
+          "profile_path": "/gtk4vSQzXNb0f8qSZPp6t9vUcSJ.jpg"
+        }
+      ]
+    },
+    {
+      "air_date": "2007-11-22",
+      "episode_number": 2,
+      "id": 256924,
+      "name": "Episode Two",
+      "overview": "Matt and Mel are told to get properties on the company's radar by any means possible. Also Danny discovers some hidden problems with his latest clients that are more involved than just buying a property. Phoebe meets her perfect man whilst being undercover at another estate agency.",
+      "production_code": "",
+      "season_number": 1,
+      "show_id": 3624,
+      "still_path": null,
+      "vote_average": 0,
+      "vote_count": 0,
+      "crew": [],
+      "guest_stars": [
+        {
+          "id": 147884,
+          "name": "Morven Christie",
+          "credit_id": "574b3932c3a36840cc000206",
+          "character": "",
+          "order": 5,
+          "gender": 1,
+          "profile_path": "/fTlG9uWyaiQt9UATyPYswjo9EGh.jpg"
+        }
+      ]
+    },
+    {
+      "air_date": "2007-11-29",
+      "episode_number": 3,
+      "id": 256925,
+      "name": "Episode Three",
+      "overview": "Mel's attraction to a future buyer is more than she bargained for, Jonty gets bullied by a newcomer, and Matt bites off more than he can chew but isn't the one who is punished for it.",
+      "production_code": "",
+      "season_number": 1,
+      "show_id": 3624,
+      "still_path": null,
+      "vote_average": 0,
+      "vote_count": 0,
+      "crew": [],
+      "guest_stars": [
+        {
+          "id": 1231651,
+          "name": "Richard Harrington",
+          "credit_id": "574b39429251412d29001a2a",
+          "character": "",
+          "order": 6,
+          "gender": 2,
+          "profile_path": "/yRiMMz91LC4EgrRpbsaw80T1XM1.jpg"
+        }
+      ]
+    },
+    {
+      "air_date": "2007-12-06",
+      "episode_number": 4,
+      "id": 256922,
+      "name": "Episode Four",
+      "overview": "Matt, ever the opportunist sees the chance to make some quick cash much to his delight and an old Gothic house gives Phoebe and Jonty an unsual experience. For Danny, he has to relive some past horrors from his childhood when he tries to sell the home he grew up in.",
+      "production_code": "",
+      "season_number": 1,
+      "show_id": 3624,
+      "still_path": null,
+      "vote_average": 0,
+      "vote_count": 0,
+      "crew": [],
+      "guest_stars": [
+        {
+          "id": 47933,
+          "name": "Adam James",
+          "credit_id": "574b39649251413f2f001a20",
+          "character": "",
+          "order": 8,
+          "gender": 2,
+          "profile_path": "/5aVa1LgnOzkzsIYdcb1QUTxV5g3.jpg"
+        }
+      ]
+    },
+    {
+      "air_date": "2007-12-13",
+      "episode_number": 5,
+      "id": 256927,
+      "name": "Episode Five",
+      "overview": "With time running out the team must secure some critical buyers otherwise the developers may look elsewhere. Danny on the other hand reunites a father and son and helps a computer programmer to find a new home.",
+      "production_code": "",
+      "season_number": 1,
+      "show_id": 3624,
+      "still_path": null,
+      "vote_average": 0,
+      "vote_count": 0,
+      "crew": [],
+      "guest_stars": []
+    },
+    {
+      "air_date": "2007-12-20",
+      "episode_number": 6,
+      "id": 256926,
+      "name": "Episode Six",
+      "overview": "Mr Colubrine announces that he is opening a branch in Spain so they all compete to get the manager position. Also they have to deal with a customer who won't leave the shop and it's time to come to terms with their feelings for Danny and Mel.",
+      "production_code": "",
+      "season_number": 1,
+      "show_id": 3624,
+      "still_path": null,
+      "vote_average": 0,
+      "vote_count": 0,
+      "crew": [],
+      "guest_stars": [
+        {
+          "id": 199522,
+          "name": "Amanda Abbington",
+          "credit_id": "574b38f09251411503000064",
+          "character": "",
+          "order": 2,
+          "gender": 1,
+          "profile_path": "/diashQMD26rrZOmj342EK8a6EaQ.jpg"
+        },
+        {
+          "id": 14465,
+          "name": "John Woodvine",
+          "credit_id": "574b3920c3a368356100038b",
+          "character": "",
+          "order": 4,
+          "gender": 2,
+          "profile_path": "/mqNKneqT3W4YEfW35BN1UJ1kJoy.jpg"
+        },
+        {
+          "id": 429541,
+          "name": "Rufus Jones",
+          "credit_id": "57813c75c3a3681387000d54",
+          "character": "",
+          "order": 9,
+          "gender": 2,
+          "profile_path": "/lqlmRVh7j6pbiu0pRZXkaoveIGJ.jpg"
+        }
+      ]
+    }
+    ],
+    "name": "Season 1",
+    "overview": "",
+    "id": 10938,
+    "poster_path": null,
+    "season_number": 1,
+    "credits": {
+    "cast": [
+      {
+        "character": "",
+        "credit_id": "5257539c19c29531db119b54",
+        "gender": 1,
+        "id": 47625,
+        "name": "Christina Cole",
+        "order": 0,
+        "profile_path": "/lOcjHQgXg5rgM6zOr4k7SsMoABV.jpg"
+      },
+      {
+        "character": "",
+        "credit_id": "5257539d19c29531db119b64",
+        "gender": 2,
+        "id": 7058,
+        "name": "Kris Marshall",
+        "order": 1,
+        "profile_path": "/gBq5B3HETUfgdK3vyXXLT7d4oXs.jpg"
+      },
+      {
+        "character": "",
+        "credit_id": "5257539d19c29531db119b74",
+        "gender": 2,
+        "id": 47703,
+        "name": "Bryan Dick",
+        "order": 2,
+        "profile_path": null
+      },
+      {
+        "character": "",
+        "credit_id": "5257539d19c29531db119b88",
+        "gender": 1,
+        "id": 118036,
+        "name": "Ella Smith",
+        "order": 3,
+        "profile_path": "/vZmsnCgeKFqvyCzot1XSWlZwGbL.jpg"
+      },
+      {
+        "character": "Mr Colubrine",
+        "credit_id": "5257539d19c29531db119b98",
+        "gender": 2,
+        "id": 34257,
+        "name": "Anthony Stewart Head",
+        "order": 4,
+        "profile_path": "/3IKmIaEQFgHtTlUI548eirwIJPP.jpg"
+      },
+      {
+        "character": "Zoe",
+        "credit_id": "5257539d19c29531db119ba8",
+        "gender": 1,
+        "id": 199522,
+        "name": "Amanda Abbington",
+        "order": 5,
+        "profile_path": "/diashQMD26rrZOmj342EK8a6EaQ.jpg"
+      }
+    ],
+    "crew": []
+    },
+    "images": {
+    "posters": []
+    },
+    "videos": {
+    "results": []
+    }
+    }
+    """.utf8)

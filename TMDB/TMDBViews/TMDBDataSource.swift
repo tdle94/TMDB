a2aa0c206193a3404278f5b426bdfa764f018872
Tuyen <tuyendle92@gmail.com>
2020-08-08 10:55:59 -0500

    add localizable

--- TMDB/TMDBViews/TMDBDataSource.swift
+++ TMDB/TMDBViews/TMDBDataSource.swift
@@ -13,11 +13,27 @@ import RealmSwift
 enum Section: String {
     case season
     case episode
+    case popular
+    case trending
+    case productionCompanies
+    case more
+    case credit
+    case video
+    case keyword
+    case image
 
     var header: String {
         switch self {
         case .season: return "Season"
         case .episode: return "Episode"
+        case .popular: return "Popular"
+        case .trending: return "Trends"
+        case .productionCompanies: return "Production Companies"
+        case .more: return "More"
+        case .credit: return "Credit"
+        case .video: return "Video"
+        case .keyword: return "Keyword"
+        case .image: return "Image"
         }
     }
 }
@@ -26,4 +42,8 @@ class TMDBTableDataSource: UITableViewDiffableDataSource<Section, Object> {
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return self.snapshot().sectionIdentifiers[section].header
     }
+}
+
+class TMDBCollectionDataSource: UICollectionViewDiffableDataSource<Section, Object> {
+
 }

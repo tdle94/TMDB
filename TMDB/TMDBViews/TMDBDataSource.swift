b82e2663d3e3ff94231a82960ae2cf6b2cb6aa64
Tuyen <tuyendle92@gmail.com>
2020-08-08 10:56:13 -0500

    add common data source

--- TMDB/TMDBViews/TMDBDataSource.swift
+++ TMDB/TMDBViews/TMDBDataSource.swift
@@ -21,19 +21,29 @@ enum Section: String {
     case video
     case keyword
     case image
+    case creator
+    case network
+    case matching
+    case backdrop
+    case appearIn
 
     var header: String {
         switch self {
-        case .season: return "Season"
-        case .episode: return "Episode"
-        case .popular: return "Popular"
-        case .trending: return "Trends"
-        case .productionCompanies: return "Production Companies"
-        case .more: return "More"
-        case .credit: return "Credit"
-        case .video: return "Video"
-        case .keyword: return "Keyword"
-        case .image: return "Image"
+        case .season: return NSLocalizedString("Season", comment: "")
+        case .episode: return NSLocalizedString("Episode", comment: "")
+        case .popular: return NSLocalizedString("Popular", comment: "")
+        case .trending: return NSLocalizedString("Trends", comment: "")
+        case .productionCompanies: return NSLocalizedString("Produce by", comment: "")
+        case .more: return NSLocalizedString("More", comment: "")
+        case .credit: return NSLocalizedString("Credit", comment: "")
+        case .video: return NSLocalizedString("Video", comment: "")
+        case .keyword: return NSLocalizedString("Keyword", comment: "")
+        case .image: return NSLocalizedString("Images", comment: "")
+        case .creator: return NSLocalizedString("Creator", comment: "")
+        case .network: return NSLocalizedString("Network", comment: "")
+        case .matching: return NSLocalizedString("More", comment: "")
+        case .backdrop: return NSLocalizedString("Images", comment: "")
+        case .appearIn: return NSLocalizedString("Appear In", comment: "")
         }
     }
 }
@@ -45,5 +55,15 @@ class TMDBTableDataSource: UITableViewDiffableDataSource<Section, Object> {
 }
 
 class TMDBCollectionDataSource: UICollectionViewDiffableDataSource<Section, Object> {
-
+    init(cellIdentifier: String, collectionView: UICollectionView) {
+        super.init(collectionView: collectionView) { collectionView, indexPath, item in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as? TMDBPreviewItemCell
+            cell?.configure(item: item)
+            return cell
+        }
+    }
+    
+    override init(collectionView: UICollectionView, cellProvider: @escaping UICollectionViewDiffableDataSource<Section, Object>.CellProvider) {
+        super.init(collectionView: collectionView, cellProvider: cellProvider)
+    }
 }

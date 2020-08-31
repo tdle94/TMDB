ee4fc18494cbecb66766254a33638eb6a8cf9702
Tuyen <tuyendle92@gmail.com>
2020-08-31 14:05:48 -0500

    view all cell

--- TMDB/TMDBViews/TMDBViewAllCell.swift
+++ TMDB/TMDBViews/TMDBViewAllCell.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+import UIKit
+
+class TMDBViewAllCell: UICollectionViewCell {
+    var label: UILabel = UILabel()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        let borderView = UIView(frame: CGRect(x: 0, y: 0, width: frame.width, height: frame.height/1.27))
+        borderView.layer.borderWidth = 0.1
+        borderView.layer.borderColor = UIColor.black.cgColor
+        borderView.layer.cornerRadius = 10
+
+        contentView.addSubview(borderView)
+        contentView.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = NSLocalizedString("View all", comment: "")
+        label.centerYAnchor.constraint(equalTo: borderView.centerYAnchor).isActive = true
+        label.centerXAnchor.constraint(equalTo: borderView.centerXAnchor).isActive = true
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}

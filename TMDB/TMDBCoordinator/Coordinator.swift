f2dea89eea797d2e8808cdc84435692d7604f534
Tuyen <tuyendle92@gmail.com>
2020-09-28 13:22:53 -0500

    add rating

--- TMDB/TMDBCoordinator/Coordinator.swift
+++ TMDB/TMDBCoordinator/Coordinator.swift
@@ -12,4 +12,31 @@ import UIKit
 protocol Coordinator {
     var childCoordinators: [Coordinator] { get set }
     var navigationController: UINavigationController { get set }
+    
+    func navigateToMovieDetail(id: Int)
+    func navigateToVideoPlayer(with videoURL: URL)
+    func navigateToReview(reivew: [Review])
+    func navigateToPersonDetail(id: Int)
+    func navigateToCompleteReleaseDates(movieId: Int)
+    func navigateToTVShowDetail(tvId: Int)
+    func navigateToTVShowSeasonDetail(tvId: Int, seasonNumber: Int)
+    func navigateToTVShowEpisodeDetail(tvId: Int, seasonNumber: Int, episodeNumber: Int)
+    func navigateToAllMovie(query: DiscoverQuery)
+    func navigateToAllTVShow(query: DiscoverQuery)
+    func navigateToAllRecommendMovie(id: Int)
+    func navigateToAllSimilarMovie(id: Int)
+    func navigateToAllPopularMovie()
+    func navigateToAllTopRatedMovie()
+    func navigateToAllUpcomingMovie()
+    func navigateToAllNowPlayingMovie()
+    func navigateToSimilarTVShow(id: Int)
+    func navigateToRecommendTVShow(id: Int)
+    func navigateToAllPopularTVShow()
+    func navigateToAllTopRatedTVShow()
+    func navigateToAllTVShowAiringToday()
+    func navigateToAllTVShowOnTheAir()
+    func navigateToAllPopularPeople()
+    func navigateToAllTrend(time: TrendingTime)
+    func presentFilter(delegate: TMDBFilterDelegate, query: DiscoverQuery, choice: TMDBFilterViewController.FilterChoice)
+    func presentRating(id: Int, ratingType: TMDBRatingViewController.RatingType, notifyRating: TMDBNotifyRating)
 }

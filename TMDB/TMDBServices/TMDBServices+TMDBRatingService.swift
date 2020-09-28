924ab6ccc2889393e9866e0ab96829c11f0d4811
Tuyen <tuyendle92@gmail.com>
2020-09-28 13:28:02 -0500

    add rating service

--- TMDB/TMDBServices/TMDBServices+TMDBRatingService.swift
+++ TMDB/TMDBServices/TMDBServices+TMDBRatingService.swift
@@ -7,3 +7,15 @@
 //
 
 import Foundation
+
+extension TMDBServices: TMDBRatingService {
+    func postMovieRating(movieId: Int, rate: Double, completion: @escaping (Result<RatingResponse, Error>) -> Void) {
+        let request = urlRequestBuilder.getPostMovieRatingURLRequest(movieId: movieId, rate: rate)
+        session.send(request: request, responseType: RatingResponse.self, completion: completion)
+    }
+    
+    func postTVShowRating(tvId: Int, rate: Double, completion: @escaping (Result<RatingResponse, Error>) -> Void) {
+        let request = urlRequestBuilder.getPostTVShowRatingURLRequest(tvId: tvId, rate: rate)
+        session.send(request: request, responseType: RatingResponse.self, completion: completion)
+    }
+}

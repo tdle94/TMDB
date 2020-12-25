fecd31894786e3f7323ed313a9940f741f49d4da
Tuyen <tuyendle92@gmail.com>
2020-12-25 13:42:33 -0500

    season detail

--- TMDB/TMDBViewControllers/TVShowSeasonDetail/TVShowSeasonDetailModel.swift
+++ TMDB/TMDBViewControllers/TVShowSeasonDetail/TVShowSeasonDetailModel.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+import RxDataSources
+
+enum TVShowSeasonDetailModel {
+    case Credits(items: [CustomElementType])
+}
+
+extension TVShowSeasonDetailModel: AnimatableSectionModelType {
+    typealias Item = CustomElementType
+    
+    typealias Identity = Int
+    
+    var identity: Int {
+        switch self {
+        case .Credits(items: let items):
+            return items.hashValue
+        }
+    }
+        
+    var items: [CustomElementType] {
+        switch self {
+        case .Credits(items: let items):
+            return items
+        }
+    }
+        
+    init(original: TVShowSeasonDetailModel, items: [CustomElementType]) {
+        switch original {
+        case .Credits(items: let items):
+            self = .Credits(items: items)
+        }
+    }
+}
--- TMDB/TMDBViewControllers/TVShowSeasonDetail/TVShowSeasonDetailViewModel.swift
+++ TMDB/TMDBViewControllers/TVShowSeasonDetail/TVShowSeasonDetailViewModel.swift
@@ -6,4 +6,121 @@
 //  Copyright © 2020 Tuyen Le. All rights reserved.
 //
 
-import Foundation
+import RxSwift
+import RealmSwift
+import Realm
+
+protocol TVShowSeasonDetailViewModelProtocol {
+    var userSetting: TMDBUserSettingProtocol { get }
+    var backdropImages: PublishSubject<[Images]> { get }
+    var season: PublishSubject<Season> { get }
+    var airDate: PublishSubject<NSAttributedString> { get }
+    var title: PublishSubject<NSAttributedString> { get }
+    var numberOfEpisode: PublishSubject<NSAttributedString> { get }
+    var overview: PublishSubject<NSAttributedString> { get }
+    var credit: BehaviorSubject<[TVShowSeasonDetailModel]> { get }
+    
+    var isThereCrew: Bool { get }
+    var isThereCast: Bool { get }
+    
+    var repository: TMDBTVShowRepository { get }
+    
+    func getSeasonDetail(tvShowId: Int, seasonNumber: Int)
+    func getSeasonCasts(tvShowId: Int, seasonNumber: Int)
+    func getSeasonCrews(tvShowId: Int, seasonNumber: Int)
+    
+    func resetCreditHeaderState()
+}
+
+class TVShowSeasonDetailViewModel: TVShowSeasonDetailViewModelProtocol {
+    
+    var userSetting: TMDBUserSettingProtocol
+    
+    var repository: TMDBTVShowRepository
+    
+    var isThereCrew: Bool = true
+    var isThereCast: Bool = true
+    
+    var backdropImages: PublishSubject<[Images]> = PublishSubject()
+    var airDate: PublishSubject<NSAttributedString> = PublishSubject()
+    var title: PublishSubject<NSAttributedString> = PublishSubject()
+    var numberOfEpisode: PublishSubject<NSAttributedString> = PublishSubject()
+    var overview: PublishSubject<NSAttributedString> = PublishSubject()
+    var season: PublishSubject<Season> = PublishSubject()
+    var credit: BehaviorSubject<[TVShowSeasonDetailModel]> = BehaviorSubject(value: [.Credits(items: [])])
+    
+    init(repository: TMDBTVShowRepository, userSetting: TMDBUserSettingProtocol) {
+        self.userSetting = userSetting
+        self.repository = repository
+    }
+    
+    func getSeasonDetail(tvShowId: Int, seasonNumber: Int) {
+        repository.getTVShowSeasonDetail(from: tvShowId, seasonNumber: seasonNumber) { result in
+            switch result {
+            case .success(let season):
+                self.season.onNext(season)
+                
+                if let airDate = season.airDate, !airDate.isEmpty {
+                    self.airDate.onNext(TMDBLabel.setAttributeText(title: NSLocalizedString("Air On", comment: ""),
+                                                                   subTitle: airDate))
+                }
+                
+                if !season.overview.isEmpty {
+                    self.overview.onNext(TMDBLabel.setAtributeParagraph(title: NSLocalizedString("Overview", comment: ""),
+                                                                        paragraph: season.overview))
+                }
+                
+                self.numberOfEpisode.onNext(TMDBLabel.setAttributeText(title: NSLocalizedString("Number Of Episode", comment: ""),
+                                                                       subTitle: String(season.episodeCount)))
+                
+                self.title.onNext(TMDBLabel.setHeader(title: season.name))
+
+                if season.credits?.cast.isEmpty ?? true, season.credits?.crew.isEmpty ?? true {
+                    self.credit.onNext([.Credits(items: [])])
+                    self.isThereCrew = false
+                    self.isThereCast = false
+                } else if season.credits?.cast.isEmpty ?? true {
+                    self.isThereCast = false
+                    self.credit.onNext([.Credits(items: Array(season.credits!.crew.map { CustomElementType(identity: $0) }) )])
+                } else if season.credits?.crew.isEmpty ?? true {
+                    self.isThereCrew = false
+                    self.credit.onNext([.Credits(items: Array(season.credits!.cast.map { CustomElementType(identity: $0) }) )])
+                } else {
+                    self.credit.onNext([.Credits(items: Array(season.credits!.cast.map { CustomElementType(identity: $0) }) )])
+                }
+
+            case .failure(let error):
+                self.season.onError(error)
+            }
+        }
+        
+        repository.getTVShowSeasonImage(from: tvShowId, seasonNumber: seasonNumber) { result in
+            switch result {
+            case .success(let imageResult):
+                let images = imageResult.backdrops.isEmpty ? imageResult.posters : imageResult.backdrops
+                self.backdropImages.onNext(Array(images))
+            case .failure(let error):
+                self.backdropImages.onError(error)
+            }
+        }
+    }
+
+    func getSeasonCasts(tvShowId: Int, seasonNumber: Int) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            let casts = self.repository.getTVShowSeasonCasts(from: tvShowId, seasonNumber: seasonNumber)
+            self.credit.onNext([.Credits(items: casts.map { CustomElementType(identity: $0) } )])
+        }
+    }
+
+    func getSeasonCrews(tvShowId: Int, seasonNumber: Int) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            let crews = self.repository.getTVShowSeasonCrews(from: tvShowId, seasonNumber: seasonNumber)
+            self.credit.onNext([.Credits(items: crews.map { CustomElementType(identity: $0) } )])
+        }
+    }
+    
+    func resetCreditHeaderState() {
+        isThereCast = true
+        isThereCrew = true
+    }
+}

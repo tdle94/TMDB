f3ca4865c533ecfded66ae472e62c1a1f1a854cb
Tuyen <tuyendle92@gmail.com>
2020-12-27 18:40:29 -0500

    person credit header view

--- TMDB/TMDBViews/CollectionViewPreviewHeaderView/TMDBPersonCreditHeaderView.swift
+++ TMDB/TMDBViews/CollectionViewPreviewHeaderView/TMDBPersonCreditHeaderView.swift
@@ -6,4 +6,13 @@
 //  Copyright © 2020 Tuyen Le. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class TMDBPersonCreditHeaderView: TMDBCreditHeaderView {
+    override func setup() {
+        segmentControl.removeAllSegments()
+        label.setHeader(title: NSLocalizedString("Credit", comment: ""))
+        segmentControl.insertSegment(withTitle: NSLocalizedString("Movies", comment: ""), at: 0, animated: true)
+        segmentControl.insertSegment(withTitle: NSLocalizedString("TV Shows", comment: ""), at: 1, animated: true)
+    }
+}

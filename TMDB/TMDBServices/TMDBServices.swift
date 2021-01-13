518503c88e43da874feb9a9cccf5609b3edb4838
Tuyen <tuyendle92@gmail.com>
2021-01-12 19:25:26 -0500

    add country

--- TMDB/TMDBServices/TMDBServices.swift
+++ TMDB/TMDBServices/TMDBServices.swift
@@ -52,6 +52,7 @@ struct DiscoverQuery: Equatable {
     var withKeyword: String?
     var withOriginalLanguage: String?
     var keywords: [Keyword] = [] // only use to save applied keywords
+    var country: String?
     
     static func == (lhs: DiscoverQuery, rhs: DiscoverQuery) -> Bool {
         return
